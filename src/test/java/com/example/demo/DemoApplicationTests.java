36750e7605afb4296a16a6aedfdde2695f2ad86c
SimonJoz <szymonjozefczuk1@gmail.com>
2021-09-17 13:22:14 +0200

    commit

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Autowired
+    private DemoApplication demoApplication;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void testHelloSuccess() {
+        assertEquals("Hello", demoApplication.hello());
+    }
+}
